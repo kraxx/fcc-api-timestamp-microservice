ddc23d1ce93d090b6547aa26206c95e43ebac732
Justin Chow <jchowchow@gmail.com>
2017-03-13 23:40:57 +0000

    up and running

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('port', process.env.PORT || 8080);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+var port = app.get('port');
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.use(express.static('public'));
+app.use(require('./routes/index'));
+
+app.get('/:datestring', function (req, res) {
+    var dateObj = dateConverter(new Date(req.params.datestring));
+    if (req.params.datestring) console.log(dateObj);
+    res.send(dateObj);
+});
+
+function dateConverter(data) {
+    return data.toDateString() === 'Invalid Date' ? JSON.stringify({'unix':null, 'natural':null})
+    : JSON.stringify({'unix':data.getTime(), 'natural':data.toDateString()});
+}
+
+app.listen(port, function () {
+  console.log('Timestamp microservice listening on port ' + port);
 })
